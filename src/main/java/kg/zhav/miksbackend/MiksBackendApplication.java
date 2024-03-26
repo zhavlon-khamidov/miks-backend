1939dab209bcbcdd5eb1483db34e99ada5270158
Mr.Zhavlon <zhavlon.khamidov@gmail.com>
2024-03-26 11:34:16 +0600

    Init project

---
+++ src/main/java/kg/zhav/miksbackend/MiksBackendApplication.java
@@ -0,0 +1,13 @@
+package kg.zhav.miksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiksBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/zhav/miksbackend/MiksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.zhav.miksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
